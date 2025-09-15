4ecb4cead2630dc413df2546cd233e49ef3d36a0
ximemolina <ximenamolinaportilla01@gmail.com>
2025-09-15 14:33:13 -0600

    Creacion ProductSchema

--- src/models/product.js
+++ src/models/product.js
@@ -1,6 +1,71 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({ });   
-
+const productSchema = new mongoose.Schema(
+  {
+    sku: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    brand: {
+      type: String
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: ["pz", "caja", "m", "kg", "lt"],
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    cost: {
+      type: Number,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    minStock: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+    location: {
+      type: String,
+    },
+    supplierId: {
+      type: String
+    },
+    tags: {
+      type: [String],
+    },
+    imageUrl: {
+      type: String
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    attributes: [ {
+        key: { type: String, required: true },
+        value: { type: String, required: true },
+    } ],
+  },
+  {
+    timestamps: true, 
+  }
+);
 
 module.exports = mongoose.model('Product', productSchema);
