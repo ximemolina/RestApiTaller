12b5150f770940a6901d0c2e860179c7aa48cd1b
ximemolina <ximenamolinaportilla01@gmail.com>
2025-09-16 16:00:29 -0600

    detalle del puerto

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const swaggerSpec = swaggerJsDoc(swaggerOptions);
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json()); //para que el servidor entienda json
